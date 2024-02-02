25e6fd23c7d1a5d8f67a343260769b2fb779fbd9
cosmicNoot <155962034+Macc-Mp@users.noreply.github.com>
2024-02-02 22:03:03 +0800

    Program for Even/Odd Num

--- EvenOdd.java
+++ EvenOdd.java
@@ -33,3 +33,26 @@ int[] num = {1,2,3,4,5,6,7,8,9,10};
                 System.out.println(number);
             }
     }
+
+//Putting sorted numbers to a single variable
+     int num2[] = new int [10];
+           for(int i=0; i<num2.length; i++){
+             System.out.print("Enter Num: ["+i+"]= ");
+             num2[i]=s.nextInt();
+         }//Array Input for 10 numbers
+           
+           //Separators
+           String evenlist="";
+           String oddlist="";
+           
+           //for looop for detecting even/odd
+           for(int ctr=0; ctr<num2.length; ctr++){
+               if(num2[ctr]%2==0){
+                   evenlist+= num2[ctr]+" ";
+               }
+               else{
+                   oddlist+= num2[ctr]+" ";
+               }
+           }
+           System.out.println("Even: " + evenlist);
+           System.out.println("Odd: " + oddlist);     
